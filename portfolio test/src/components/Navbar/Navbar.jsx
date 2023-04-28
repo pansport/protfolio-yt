85b698669f3a4916fed8526e4a6be23911e5c023
Kristijan Horvatić <111249835+pansport@users.noreply.github.com>
2023-04-28 11:37:55 +0200

    Add files via upload

---
+++ portfolio test/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+export default function Navbar() {
+  const [click, setClick] = useState(false);
+  const [color, setColor] = useState(false);
+
+  const handleClick = () => {
+    setClick((prevClick) => !prevClick);
+  };
+
+  const changeColor = () => {
+    if (window.scrollY >= 1) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeColor);
+
+  return (
+    <nav className={`navbar ${color ? "navbar-bg" : ""}`}>
+      <h1>Portfolio.</h1>
+      <ul className={`menu-links ${click ? "active" : ""}`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+        <li>
+          <Link to="/project">About</Link>
+        </li>
+        <li>
+          <Link to="/project">Contact</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
+      </div>
+    </nav>
+  );
+}
---
+++ portfolio test/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+import Hero from "../../components/Hero/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
